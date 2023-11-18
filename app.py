f86e7a9e402ce843a23765ec0bea0fc1898f5926
Colin <colin4554@users.noreply.huggingface.co>
2023-11-18 03:29:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
+from openai import OpenAI
 import streamlit as st
-import numpy as np
+import os
+from openai import AzureOpenAI
 
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
+st.title("Support Chat UI")
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(prompt)
+client = AzureOpenAI(
+  api_key = os.environ['OPENAI_API_KEY'],  
+  api_version = "2023-07-01-preview",
+  azure_endpoint = os.environ['AZURE_ENDPOINT'],  
+)
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-35-turbo-0613"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
